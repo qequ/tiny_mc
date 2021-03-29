d14f74becfc2e432f465125aa90a54665b3ffb45
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-03-29 13:05:16 -0300

    Actualizado bash script de compilado

--- automate_compilation.sh
+++ automate_compilation.sh
@@ -1,4 +1,13 @@
-cat parameters.txt | while read p; do for ((i=1; i<=30; i=i+1)); do
-make clean && 
-make EXTRA_CFLAGS="${p}" CPP_FLAGS="-DPHOTONS=$((1 + $RANDOM % 32768))" && 
-./tiny_mc; done; done
+#!/bin/bash
+
+for ((i = 1; i <= 30; i = i + 1)); do
+    echo $((1 + RANDOM % 32768)) >>sample.txt
+done
+
+cat parameters.txt | while read p; do cat sample.txt | while read m; do
+    make clean &&
+        make EXTRA_CFLAGS="${p}" CPPFLAGS="-DPHOTONS=${m}" &&
+        ./tiny_mc
+done; done
+
+rm -rfv sample.txt
