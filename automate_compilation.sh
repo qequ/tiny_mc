2573f32bcf536bcd1d0955571e9d1551573c4580
Ary Lautaro Di Bartolo <lautarodibartolo@gmail.com>
2021-04-01 11:19:03 -0300

    Comentarios en el script de automatización y en el cálculo de la media.

--- automate_compilation.sh
+++ automate_compilation.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+#¿Esto genera 30 números aleatorios y los guarda en un archivo de texto?
+
 for ((i = 1; i <= 30; i = i + 1)); do
     echo $((1 + RANDOM % 32768)) >>sample.txt
 done
+
+#No entiendo qué hace esta línea.
 rm -rfv means.txt && touch means.txt
 
+#Lo que hace esto es leer líneas de parameters.txt que son tipos de compilaciones. 
+#A cada tipo de compilación le hace correr tiny_mc con los 30 números aleatorios generados. 
+#Los resultados los guarda en un archivo de texto.
+#Mi pregunta es: ¿por qué no poner las extra flags directamente en el archivo parameters.txt?
+#Después calcula la media de los resultados de la métrica con los 30 números aleatorios generados para la compilación.
 cat parameters.txt | while read p; do
     cat sample.txt | while read m; do
         make clean && make EXTRA_CFLAGS="${p}" CPPFLAGS="-DPHOTONS=${m}" && ./tiny_mc >>photons_results.txt
@@ -13,4 +22,5 @@ cat parameters.txt | while read p; do
     echo python;python3 mean_calc.py photons_results.txt "${p}"; rm -rfv photons_results.txt
 done
 
+#No entiendo qué hace esta línea.
 rm -rfv sample.txt
--- mean_calc.py
+++ mean_calc.py
@@ -1,11 +1,20 @@
+#Importamos las librerías necesarias.
 import sys
 import statistics
 
+#Abrimos el archivo means.txt generado con el modo append (porque le queremos agregar nueva información al final).
+#Lo llamamos mf.
 with open("means.txt", "a") as mf:
 
+#Abrimos el archivo sys.argv[1] con el modo de sólo lectura. Lo llamamos f.
+#¿Qué es el archivo sys.argv[1]?
     with open(sys.argv[1], "r") as f:
+#Guardamos en una variable auxiliar llamada data algo.
+#¿Ese algo qué sería?
         data = f.read().split("\n")
 
+#Esto guarda en photons_per_second algo.
     photons_per_second = list(map(float, data[:-1]))
+#Se escribe en mf la media de los datos.
     mf.write("{}: {}\n".format(
         sys.argv[2], statistics.mean(photons_per_second)))
