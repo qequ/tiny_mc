a4d50a44220ef14ee2735cf4f5fa277fb9978f1e
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-05-29 14:46:39 -0300

    añadido script de scaling para omp vectorizado

---
+++ scaling_omp_vec.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for ((i = 1; i <= 64; i = i + 1)); do
+    echo $(("1024*i")) >>scaling_sample.txt
+done
+
+rm -rfv means.txt
+touch means.txt
+
+cat scaling_sample.txt | while read m; do
+
+    for i in {1..30}; do
+        make clean && make omp_vector CPPFLAGS="-DPHOTONS=${m}" && ./tiny_mc_vectorized_omp;
+    done
+
+    echo python; python3 mean_calc.py photons_results.txt "${m}"; rm -rfv photons_results.txt
+
+
+done
+
+
+python3 plot_means.py "scaling"
+
+rm -rfv scaling_sample.txt
+
