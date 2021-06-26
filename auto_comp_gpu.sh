14bc3545d6bb737da49c1502a888f9d389e9622f
Álvaro Frías Garay <cp202146@zx81.famaf.unc.edu.ar>
2021-06-26 11:30:55 -0300

    actualizado script de compilacion

--- auto_comp_gpu.sh
+++ auto_comp_gpu.sh
@@ -11,7 +11,7 @@ rm -rfv photons_results.txt
 touch photons_results.txt
 
 cat sample.txt | while read m; do
-    make clean && make gpu CPPFLAGS="-DPHOTONS=${m}" && ./tiny_mc_gpu;
+    nvcc -O3 -use_fast_math -DPHOTONS=${m} -o tiny_mc_gpu tiny_mc_shared_block.cu  && ./tiny_mc_gpu;
 done
 
 echo python; python3 mean_calc.py photons_results.txt "gpu"; rm -rfv photons_results.txt
