fe28741b7e9c8d06e52344ff8872636ec6d523f6
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-06-26 01:37:45 -0300

    añadido script para automatizar compilacion

---
+++ auto_comp_gpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for ((i = 1; i <= 100; i = i + 1)); do
+    echo $((1 + RANDOM % 65536)) >>sample.txt
+done
+
+rm -rfv means.txt
+touch means.txt
+
+rm -rfv photons_results.txt
+touch photons_results.txt
+
+cat sample.txt | while read m; do
+    make clean && make gpu CPPFLAGS="-DPHOTONS=${m}" && ./tiny_mc_gpu;
+done
+
+echo python; python3 mean_calc.py photons_results.txt "gpu"; rm -rfv photons_results.txt
+
+
+rm -rfv sample.txt
